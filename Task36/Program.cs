35d71c8833f1430f00fb020dc9a9eea32f76ebb4
Engineer <arsenya.suvorov@mail.ru>
2022-12-01 10:49:28 +0400

    start

--- Task36/Program.cs
+++ Task36/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] FillArray(int size)
+    {
+        int[] arr =new int[size];
+        for(int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = new Random().Next(0,200);
+            }
+            return arr;
+    }
+
+Console.Write("Введите длину массива: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int[] array = FillArray(num);
+int sum = 0;
+Console.WriteLine(string.Join(", ", array));
+
+for(int i =0; i< array.Length; i++)
+    {
+        if(array[i]>=10 && array[i]<=99)
+            {
+                sum=sum+1;
+            }
+        else{}
+    }
+Console.WriteLine(sum); 
